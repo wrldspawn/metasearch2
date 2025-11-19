f04f35116aff42475d42c8ace5a77e61d2c864c5
worldspawn <worldspawn@riseup.net>
2025-11-18 17:49:28 -0700

    ecosia

---
+++ src/engines/search/ecosia.rs
@@ -0,0 +1,30 @@
+use url::Url;
+
+use crate::{
+    engines::{EngineResponse, RequestResponse, CLIENT},
+    parse::{parse_html_response_with_opts, ParseOpts},
+};
+
+pub fn request(query: &str) -> RequestResponse {
+    CLIENT
+        .get(
+            Url::parse_with_params(
+                "https://www.ecosia.org/search",
+                &[("method", "index"), ("q", query)],
+            )
+            .unwrap()
+            .as_str(),
+        )
+        .into()
+}
+
+pub fn parse_response(body: &str) -> eyre::Result<EngineResponse> {
+    parse_html_response_with_opts(
+        body,
+        ParseOpts::new()
+            .result(".result__body")
+            .title(".result-title__heading")
+            .href(".result__link")
+            .description(".web-result__description"),
+    )
+}
